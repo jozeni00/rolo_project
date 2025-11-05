743ff3b424c8067fe7ec8129b6ce309c824d7e8b
gillarmanjit-spec <gill.armanjit@gmail.com>
2025-11-04 16:06:46 -0800

    Added a detection radius
    When the player enters the radius, the enemy gives chase. Once the player leaves, the enemy gives up. The theoretical next step is to make the enemy wait before giving up so the player cannot escape too easily

--- enemy.gd
+++ enemy.gd
@@ -7,6 +7,7 @@ const RIGHT = Vector2(1,1)
 var velocity
 var player
 @export var rad = 40
+var aggro
 
 @onready var sprite := $enemSprite
 
@@ -18,26 +19,28 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var pDist = (player.position[0]^2)
-	
-	chase(player.position, delta)
-	#velocity = 0
-	position += velocity * delta
-	if ((velocity.length() > 0)):
-		sprite.play("walk")
-	
-		if(velocity[0] < 0 and self.scale != LEFT):# and self.get_child(2).cursor_position.x < self.position.x):
-			
-			self.scale = LEFT
-		elif(velocity[0] > 0 and self.scale == LEFT):
-			self.scale = RIGHT
+	if(aggro):
+		chase(player.position, delta)
+		#velocity = 0
+		position += velocity * delta
+		if ((velocity.length() > 0)):
+			sprite.play("walk")
 		
+			if(velocity[0] < 0 and self.scale != LEFT):# and self.get_child(2).cursor_position.x < self.position.x):
+				
+				self.scale = LEFT
+			elif(velocity[0] > 0 and self.scale == LEFT):
+				self.scale = RIGHT
+			
 	else:
+		speed = 40
 		sprite.play("idle")
-		#print("Nothin at all")
-	pass
+			#print("Nothin at all")
+		pass
 	
 func chase(player, delta: float):
+	if(speed < 180):
+		speed += 1
 	var direction = (player - global_position).normalized()
 	velocity = direction * speed
 	
@@ -45,3 +48,17 @@ func chase(player, delta: float):
 func check_move() -> bool:
 	# Check ahead for collision
 	return true
+
+
+func _on_detection_area_entered(area: Area2D) -> void:
+	if area == player:
+		print("IT BE THE PLAYER")
+		aggro = true
+	pass # Replace with function body.
+
+
+func _on_detection_area_exited(area: Area2D) -> void:
+	if area == player:
+		print("Must've been the wind...")
+		aggro = false
+	pass # Replace with function body.
