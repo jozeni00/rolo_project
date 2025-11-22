4df427fa43fd9a1e49747e06b8dc3447dd15102e
cisty <cistyvue@yahoo.com>
2025-11-21 17:59:32 -0800

    Fixed character & enemy broken dependencies.

--- src/main/main.gd
+++ src/main/main.gd
@@ -9,7 +9,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_viewport().size = screensize
-	self.get_child(2).get_child(0).player = self.get_child(0)
+	#self.get_child(2).get_child(0).player = self.get_child(0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
